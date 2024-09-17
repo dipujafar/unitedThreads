048b9b4151abd3b9a72f6beeba0d0e189ca40322
unknown <dipujafarmr@gmail.com>
2024-09-17 21:08:12 +0600

    fix a design

--- src/components/(auth)/verifyEmail/VerifyEmailForm.tsx
+++ src/components/(auth)/verifyEmail/VerifyEmailForm.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import type { FormProps } from "antd";
-import { Button, Form, Input } from "antd";
+import { Button, ConfigProvider, Form, Input } from "antd";
 import { useRouter } from "next/navigation";
 
 type FieldType = {
@@ -13,46 +13,56 @@ const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
 };
 
 const VerifyEmailForm = () => {
-
   const router = useRouter();
 
   const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
     console.log(values);
-    router.push("/setNewPass")
+    router.push("/setNewPass");
   };
 
   return (
-    <Form
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-      layout="vertical"
-      className="md:w-[481px]"
-      
+    <ConfigProvider
+      theme={{
+        components: {
+          Form: {
+            labelColor: "rgb(248,250,252)",
+          },
+        },
+      }}
     >
-      <Form.Item<FieldType>
-        label="OTP"
-        name="otp"
-        style={{display: "flex", justifyContent: "center", alignItems: "center"}}
-        rules={[
-          { required: true, message: "Please input OPT" },
-        ]}
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        layout="vertical"
+        className="md:w-[481px]"
       >
-        <Input.OTP size="large" length={4}/>
-      </Form.Item>
-
-      <Form.Item style={{ display: "flex", justifyContent: "center" }}>
-        <Button
-          htmlType="submit"
-          size="large"
-          style={{ backgroundColor: "#232323", color: "#F8FAFC" }}
+        <Form.Item<FieldType>
+          label="OTP"
+          name="otp"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          rules={[{ required: true, message: "Please input OPT" }]}
         >
-          Verify 
-        </Button>
-      </Form.Item>
-    </Form>
+          <Input.OTP size="large" length={4} />
+        </Form.Item>
+
+        <Form.Item style={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            htmlType="submit"
+            size="large"
+            style={{ backgroundColor: "#232323", color: "#F8FAFC" }}
+          >
+            Verify
+          </Button>
+        </Form.Item>
+      </Form>
+    </ConfigProvider>
   );
 };
 
